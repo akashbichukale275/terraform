732016d785d3f5bc9ca808149136482349f5e4b6
akashbichukale275 <166936334+akashbichukale275@users.noreply.github.com>
2024-06-12 15:02:44 +0530

    ss

--- instance/instance.tf
+++ instance/instance.tf
@@ -9,3 +9,22 @@ resource "aws_iam_user" "lb" {
     tag-key = "tag-value"
   }
 }
+resource "aws_iam_user" "lb_user" {
+  user_name = "lb"
+}
+
+resource "aws_iam_user_policy" "lb_admin_policy" {
+  user_name = aws_iam_user.lb_user.user_name
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "iam:PassRole"  <-- Replace with specific actions
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
